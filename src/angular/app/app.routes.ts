0c2c1c29b42ae33508af47c30ef6d697bcee4f9a
Maxim Kalinovskiy <malekylik@gmail.com>
2018-11-10 11:42:58 +0300

    rule-page/refactoring: moved routing at app routes module

--- src/angular/app/app.routes.ts
+++ src/angular/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from "@angular/router";
 
 import { RulesListPageComponent } from './modules/rules-list/components/rules-list-page/rules-list-page.component';
+import { ListComponent } from './modules/rules-list/components/list/list.component';
+import { RulePageComponent } from './modules/rule/components/rule-page/rule-page.component';
 
 const ROUTES: Routes = [
     { path: '',   redirectTo: 'list', pathMatch: 'full' },
-    { path: 'list', component: RulesListPageComponent },
+    { 
+        path: 'list',
+        component: RulesListPageComponent,
+        children: [
+            { path: '',     component: ListComponent },
+            { path: 'new',  component: RulePageComponent },
+        ],
+    },
 ];
 
 export {
--- src/angular/app/modules/rules-list/rules-list.module.ts
+++ src/angular/app/modules/rules-list/rules-list.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { RuleModule } from '../rule/rule.module';
 import { ListComponent } from './components/list/list.component';
 import { RulesListService } from './services/rules-list/rules-list.service';
-import { ROUTES } from './rules-list.routes';
 import { RulesListPageComponent } from './components/rules-list-page/rules-list-page.component';
 
 @NgModule({
@@ -14,7 +12,6 @@ import { RulesListPageComponent } from './components/rules-list-page/rules-list-
     CommonModule,
     SharedModule,
     RuleModule,
-    RouterModule.forChild(ROUTES)
   ],
   providers: [
     RulesListService
