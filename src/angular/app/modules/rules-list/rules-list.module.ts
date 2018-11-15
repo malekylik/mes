fc907d75e44ee2f17f58ab8e4654df9d9f75e5e7
Maxim Kalinovskiy <malekylik@gmail.com>
2018-11-15 18:36:35 +0300

    diagnostic-page/reafactoring: moved list routing to its module

--- src/angular/app/modules/rules-list/rules-list.module.ts
+++ src/angular/app/modules/rules-list/rules-list.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
 import { RuleModule } from '../rule/rule.module';
@@ -7,12 +8,14 @@ import { ListComponent } from './components/list/list.component';
 import { RulesListService } from './services/rules-list/rules-list.service';
 import { RulesListPageComponent } from './components/rules-list-page/rules-list-page.component';
 import { EditRulePageComponent } from './components/edit-rule-page/edit-rule-page.component';
+import { ROUTES } from './rules-list.routes';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
     RuleModule,
+    RouterModule.forChild(ROUTES),
   ],
   providers: [
     RulesListService
