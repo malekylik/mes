e262f49cfdec7639ecce3433bcef0d23edfaf7c5
Maxim Kalinovskiy <malekylik@gmail.com>
2018-11-24 17:34:46 +0300

    inference-engine/feat: created base inference rule

--- src/angular/app/modules/inference/inference-rules/age-leukocytosis-time.ts
+++ src/angular/app/modules/inference/inference-rules/age-leukocytosis-time.ts
@@ -1,41 +1,19 @@
-import { Cursor } from 'mongodb';
-
-import { InferenceRule } from './inference-rule.model';
+import { BaseInferenceRule } from './base-inference-rule';
 import { Rule } from 'src/electron/interfaces/Rule';
-import { DiagnosisInfo } from 'src/electron/interfaces/DiagnosisInfo';
+import { map } from 'src/angular/app/utils/interfaces/map';
 
-export class AgeLeukocytosisTime implements InferenceRule {
-    async inference(rulesDb: any, rule: Rule): Promise<DiagnosisInfo[]> {
-        const cursor: Cursor = await rulesDb.aggregate([
-            {
-                "$match": {
-                    age: rule.age,
-                    t: rule.t,
-                    "oak.L": rule.oak.L,
-                }
-            },
-            {
-                "$group": {
-                    _id: "$diagnosis",
-                    rules: {
-                        "$push": {
-                            _id: "$_id",
-                            name: "$name",
-                        }
-                    },
-                    count: { "$sum": 1 },
-                }
-            }
-        ]);
-
-        const rulesPromises = [];
-  
-        while (await cursor.hasNext()) rulesPromises.push(cursor.next());
-
-        return Promise.all(rulesPromises);
-    }
-
+export class AgeLeukocytosisTime extends BaseInferenceRule {
     toString(): string {
         return "Age-Leukocytosis-Time";
+    }
+
+    protected getMathcher(rulesDb: any, rule: Rule): map<any> {
+        return {
+            "$match": {
+                age: rule.age,
+                t: rule.t,
+                "oak.L": rule.oak.L,
+            }
+        };
     }
 }
